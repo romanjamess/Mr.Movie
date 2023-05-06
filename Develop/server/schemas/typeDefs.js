4a4d50e72045967b6b7a2ab5369b4c2682f870ba
romanjamess <118143164+romanjamess@users.noreply.github.com>
2023-05-06 18:01:36 -0500

    fixed error with graphql

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,12 +1,17 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+
 type User {
   _id: ID!
   username: String!
   email: String!
   password: String!
 
+}
+
+type Query {
+    me: User
 }
 
 `
